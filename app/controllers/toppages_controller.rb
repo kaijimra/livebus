45816ad3365458579c775363559be974bec87a14
kaiji.iimura <sky_ji@jt8.so-net.ne.jp>
2021-09-24 09:50:11 +0000

    skel completed

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,7 +1,8 @@
 class ToppagesController < ApplicationController
   def index
     if logged_in?
-      @pagy, @stops = pagy(Stop.order(id: :desc))
+      @simtime = format_time(Time.at(Time.now.to_i*600))
+      @pagy, @stops = pagy(Stop.order(id: :asc))
     else
       redirect_to login_path
     end
